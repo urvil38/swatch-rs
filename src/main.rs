2d32a9f05cf346c5d60a2ca007e47e70a4d7ea3a
Urvil Patel <patelurvil38@gmail.com>
2021-02-03 23:28:17 +0530

    Add lib module

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,23 @@ use std::io::{self, Write};
 use std::path::Path;
 use structopt::StructOpt;
 mod lib;
-use lib::{Pixel, quantize, most_variant_color, order_by_luminance};
+use lib::{order_by_luminance, quantize, Pixel};
 
-use image::GenericImageView;
+use image::{GenericImageView, ImageBuffer, RgbImage};
 
 #[derive(Debug)]
-enum OutputType {
+enum DebugType {
     JSON,
     HTML,
-    FILE
+    FILE,
 }
 
-fn parse_output_type(s: &str) -> Result<OutputType, String> {
+fn parse_output_type(s: &str) -> Result<DebugType, String> {
     match s {
-        "html" => Ok(OutputType::HTML),
-        "json" => Ok(OutputType::JSON),
-        "file" => Ok(OutputType::FILE),
-        _ => Err(format!(
-            "Invalid value provided for output {}. value can be html | json | file",
-            s
-        )),
+        "html" => Ok(DebugType::HTML),
+        "json" => Ok(DebugType::JSON),
+        "file" => Ok(DebugType::FILE),
+        _ => Err(format!("{}. value can be (html | json | file)", s)),
     }
 }
 
@@ -34,26 +31,28 @@ fn parse_output_type(s: &str) -> Result<OutputType, String> {
     about = "utility to quantize image to N dominant color using median cut algorithm."
 )]
 struct Opt {
-    /// Image file path
-    #[structopt(short = "i", long = "image", default_value = "")]
+    /// Absolute path to an image
+    #[structopt(short = "i", long = "image")]
     image_path: String,
 
-    /// Max depth
-    #[structopt(short = "d", long = "max-depth", default_value = "4")]
-    max_depth: i64,
+    /// Number of colors needed in power of 2, ex: for 16 colors pass 4. (i.e. 2^4 = 16)
+    #[structopt(short = "c", long = "colors", default_value = "4")]
+    max_depth: u32,
 
-    /// Output type
-    #[structopt(short = "o", long = "output", parse(try_from_str = parse_output_type), default_value = "html", case_insensitive = true)]
-    output: OutputType,
+    /// Debug type print quantized pixels to given format. value can be (html, json or file). "file" debug type will create a "swatch.html" and write HTML data into it.
+    #[structopt(short = "d", long = "debug-type", parse(try_from_str = parse_output_type), case_insensitive = true)]
+    debug_type: Option<DebugType>,
+
+    #[structopt(
+        short = "o",
+        long = "output-image",
+        default_value = "quantize-image.png"
+    )]
+    output: String,
 }
 
 fn main() -> Result<(), io::Error> {
     let opt = Opt::from_args();
-
-    if opt.image_path == "" {
-        println!("error: please provide absolute path of an image!");
-        process::exit(1);
-    }
 
     let image_path = Path::new(&opt.image_path);
     let img = image::open(image_path);
@@ -68,41 +67,50 @@ fn main() -> Result<(), io::Error> {
     let pixel_count = img.pixels().count();
 
     let mut pixels: Vec<Pixel> = Vec::with_capacity(pixel_count);
+    let mut img_buf: RgbImage = ImageBuffer::new(img.width(), img.height());
 
     for pixel in img.pixels() {
         let p = Pixel {
             r: pixel.2[0] as i64,
             g: pixel.2[1] as i64,
             b: pixel.2[2] as i64,
+            x: pixel.0 as i64,
+            y: pixel.1 as i64,
         };
         pixels.push(p);
     }
 
-    let mut pixels = quantize(&mut pixels, 0, opt.max_depth as usize);
+    let mut pixels = quantize(&mut pixels, opt.max_depth as usize, &mut img_buf);
+
+    img_buf.save(opt.output).unwrap();
+
     order_by_luminance(&mut pixels);
 
-    write_to(opt.output, &pixels, filename.to_string());
+    match opt.debug_type {
+        None => (),
+        Some(d) => write_to(d, &pixels, filename.to_string())
+    }
 
     Ok(())
 }
 
-fn write_to(output_type: OutputType, pixels: &Vec<Pixel>, filename: String) {
+fn write_to(output_type: DebugType, pixels: &Vec<Pixel>, filename: String) {
     match output_type {
-        OutputType::HTML | OutputType::FILE => print_html(pixels, filename, output_type).unwrap(),
-        OutputType::JSON => print_json(pixels).unwrap(),
+        DebugType::HTML | DebugType::FILE => print_html(pixels, filename, output_type).unwrap(),
+        DebugType::JSON => print_json(pixels).unwrap(),
     }
 }
 
 fn print_html(
     pixels: &Vec<Pixel>,
     filename: String,
-    output_type: OutputType,
+    output_type: DebugType,
 ) -> Result<(), io::Error> {
     let html = r#"<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>{{:Title:}}</title>
+        <title>{{:TITLE:}}</title>
         <style>
             html, body { width: 100%; height: 100%; margin: 0; padding: 0}
             body { display: flex; flex-wrap: wrap;}
@@ -110,19 +118,11 @@ fn print_html(
         </style>
     </head>
     <body>"#;
-    let primary = most_variant_color(&pixels);
 
     let mut body = String::new();
-    body.push_str(&html.replace("{{:Title:}}", filename.as_ref()));
+    body.push_str(&html.replace("{{:TITLE:}}", filename.as_ref()));
     body.push('\n');
-    body.push_str(&format!(
-        "\t<div class=\"color\" style=\"background-color: rgb({},{},{})\"></div>\n",
-        primary.r, primary.g, primary.b
-    ));
     for p in pixels {
-        if *p == primary {
-            continue;
-        }
         body.push_str(&format!(
             "\t<div class=\"color\" style=\"background-color: rgb({},{},{})\"></div>\n",
             p.r, p.g, p.b
@@ -130,8 +130,8 @@ fn print_html(
     }
     body.push_str("</body>\n</html>");
     match output_type {
-        OutputType::HTML => io::stdout().write_all(body.as_bytes())?,
-        OutputType::FILE => fs::write("swatch.html", body.as_bytes())?,
+        DebugType::HTML => io::stdout().write_all(body.as_bytes())?,
+        DebugType::FILE => fs::write("swatch.html", body.as_bytes())?,
         _ => (),
     }
 
